da51fb706aed088d2d434ec1bffaa73986bb316a
Travis Johnson <travis@thisguy.codes>
2015-02-04 15:58:14 -0800

    first run at setup.py

---
+++ setup.py
@@ -0,0 +1,74 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'ngSe', 'VERSION'), encoding='utf-8') as f:
+    version = f.read().strip()
+
+setup(
+    name='ngSe',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version=version,
+
+    description='A Selenium browser abstraction for modern apps',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/conslo/ngSe',
+
+    # Author details
+    author='Travis Johnson',
+    author_email='travis@thisguy.codes',
+
+    # Choose your license
+    license='LGPLv2.1',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords='testing selenium browser chrome',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    package_data={
+        'ngSe': ['VERSION'],
+    },
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'selenium>=2.44.0,<3'
+    ]
+)
