b087822f0fe5216547eada5a070e56ad9b29137c
Travis Johnson <travis@thisguy.codes>
2015-02-04 16:37:02 -0800

    slice off AppPage

--- ngSe/browser.py
+++ ngSe/browser.py
@@ -14,31 +14,9 @@ from .contract import must_be
 from .by import By, ByClause
 from .utils import retry
 from .exceptions import NavigationError, WaitFailedError, element_exceptions, DontRetryError, FrontEndError, cant_see_exceptions
+from .page import AppPage
 
 download_directory = "./tmp"
-
-
-class AppPage(object):
-
-    """Object to represent pages to navigate to in the app
-    """
-
-    def __init__(self, page, wait_for=None, wait_for_by=By.ID):
-        # Contract
-        if not isinstance(page, basestring) and not hasattr(page, "__call__"):
-            raise ValueError("page must be a string or callable")
-        must_be(wait_for, "wait_for", (NoneType, basestring))
-        must_be(wait_for_by, "wait_for_by", (ByClause))
-        #
-        self._page = page
-        self.wait_for = wait_for
-        self.wait_for_by = wait_for_by
-
-    @property
-    def page(self):
-        if not isinstance(self._page, basestring) and hasattr(self._page, "__call__"):
-            return self._page()
-        return self._page
 
 
 class Browser(Chrome):
---
+++ ngSe/page.py
@@ -0,0 +1,27 @@
+from types import NoneType
+
+from .by import ByClause
+from .contract import must_be
+
+
+class AppPage(object):
+
+    """Object to represent pages to navigate to in the app
+    """
+
+    def __init__(self, page, wait_for=None, wait_for_by=By.ID):
+        # Contract
+        if not isinstance(page, basestring) and not hasattr(page, "__call__"):
+            raise ValueError("page must be a string or callable")
+        must_be(wait_for, "wait_for", (NoneType, basestring))
+        must_be(wait_for_by, "wait_for_by", (ByClause))
+        #
+        self._page = page
+        self.wait_for = wait_for
+        self.wait_for_by = wait_for_by
+
+    @property
+    def page(self):
+        if not isinstance(self._page, basestring) and hasattr(self._page, "__call__"):
+            return self._page()
+        return self._page
